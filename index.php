104ab1067f191b61c0ce3cae1f976e23bbb70fe6
vishwakranti <87359394+vishwakranti@users.noreply.github.com>
2022-01-27 23:45:26 +1300

    layout changes
    changed index page layout. Added user login page. Cleaned up the nav links

--- index.php
+++ index.php
@@ -5,20 +5,13 @@
    <head>
       <meta charset="utf-8">
       <meta name="viewport" content="width=device-width, initial-scale=1">
-      <meta name="description" content="">
-      <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
-      <meta name="generator" content="Hugo 0.84.0">
+      <meta name="description" content="Carland Motors - Find the best car for you!">
+      <meta name="author" content="Vishwakranti Suryawanshi">
       <title>Carland Motors</title>
       <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/carousel/">
       <!-- Bootstrap core CSS -->
       <link href="https://getbootstrap.com/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-      <!-- Favicons -->
-      <link rel="apple-touch-icon" href="https://getbootstrap.com/docs/5.0/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
-      <link rel="icon" href="https://getbootstrap.com/docs/5.0/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
-      <link rel="icon" href="https://getbootstrap.com/docs/5.0/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
-      <link rel="manifest" href="https://getbootstrap.com/docs/5.0/assets/img/favicons/manifest.json">
-      <link rel="mask-icon" href="https://getbootstrap.com/docs/5.0/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
-      <link rel="icon" href="http://localhost:81/carlandmotors_Vishwakranti/favicon.ico">
+      <link rel="icon" href="favicon.ico">
       <meta name="theme-color" content="#7952b3">
       <style>
          .bd-placeholder-img {
@@ -37,68 +30,28 @@
          }
          }
       </style>
-      <!-- Custom styles for this template -->
-      <!--<link href="https://getbootstrap.com/docs/5.0/examples/carousel/carousel.css" rel="stylesheet">-->
    </head>
    <body>
-      <!--<header class="mt-0">
-         <nav class="navbar navbar-expand-md navbar-light bg-light ">
-             <div class="d-flex me-auto justify-content-center ms-auto">
-                 <a class="navbar-brand" href="#"><img src="./images/cars_logo.png" alt="" width="150" height="100"></a>
-                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                     <span class="navbar-toggler-icon"></span>
-                 </button>
-                 <div class="collapse navbar-collapse" id="navbarCollapse">
-                     <ul class="navbar-nav ">
-                         <li class="nav-item">
-                             <a class="nav-link active" aria-current="page" href="#">Home</a>
-                         </li>
-                         <li class="nav-item">
-                             <a class="nav-link" href="#">Link</a>
-                         </li>
-                         <li class="nav-item">
-                             <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Another Link</a>
-                         </li>
-                         <li class="nav-item dropdown">
-                             <a class="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">Account</a>
-                             <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                 <li><a class="dropdown-item" href="/login">Login</a></li>
-                                 <li><a class="dropdown-item" href="/register">Register</a></li>
-                             </ul>
-                         </li>
-                     </ul>
-                     <form class="d-flex justify-content-center me-auto">
-                         <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                         <button class="btn btn-outline-success" type="submit">Search</button>
-                     </form>
-                 </div>
-             </div>
-         </nav>
-         </header>-->
       <header class="mt-0">
          <nav class="navbar navbar-expand-md navbar-light bg-light justify-content-center">
             <div class="d-flex">
-               <a class="navbar-brand" href="#"><img src="./images/cars_logo.png" alt="" width="150" height="100"></a>
+               <a class="navbar-brand" href="."><img src="./images/cars_logo.png" alt="" width="200" height="100" alt="Carland Motors"></a>
                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
                <span class="navbar-toggler-icon"></span>
                </button>
                <div class="collapse navbar-collapse" id="navbarCollapse">
                   <ul class="nav nav-pills">
                      <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                     </li>
-                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Login</a>
-                        <ul class="dropdown-menu">
-                           <li><a class="dropdown-item" href="#">User Login</a></li>
-                           <li>
-                              <hr class="dropdown-divider">
-                           </li>
-                           <li><a class="dropdown-item" href="#">Admin Login</a></li>
-                        </ul>
+                        <a class="nav-link active" aria-current="page" href="./">Home</a>
                      </li>
                      <li class="nav-item">
-                        <a class="nav-link" href="#">Wish list</a>
+                        <a class="nav-link" aria-current="page" href="login.php">User Login</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="./admin/admin.php">Admin Login</a>
+                     </li>
+                     <li class="nav-item">
+                        <a class="nav-link" href="wishList.php">Wish list</a>
                      </li>
                      <li class="nav-item">
                         <form class="d-flex">
@@ -112,10 +65,7 @@
          </nav>
       </header>
       <main>
-         <!-- Marketing messaging and featurettes
-            ================================================== -->
-         <!-- Wrap the rest of the page in another container to center all the content. -->
-         <div class="container marketing mt-0">
+         <div class="container mt-0">
             <div class="row">
                <div class="col-sm-4">
                   <div class="card">
@@ -183,9 +133,9 @@
          </div>
          <!-- /.container -->
          <!-- FOOTER -->
-         <footer class="container">
-            <p class="float-end"><a href="#">Back to top</a></p>
-            <p>&copy; 2022 Carland Motors, Inc. &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></p>
+         <footer class="container mt-0">
+            <p class="float-end"><a href="#">Site designed by Vishwakranti Suryawanshi</a></p>
+            <p>&copy; 2022 Carland Motors Inc. </p>
          </footer>
       </main>
       <script src="https://getbootstrap.com/docs/5.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
