0c86051f41640655438b3c914fae00de09dd8f04
vishwakranti <87359394+vishwakranti@users.noreply.github.com>
2022-02-27 09:18:40 +1300

    get cars from database
    added logic to get cars from the database

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
+<?php
+
+//load database connections
+require_once "config.php";
+$cars ="";
+
+if($_SERVER["REQUEST_METHOD"] == "GET")
+{
+
+	//prepare sql statement
+	$sql = "SELECT car.price,car.year, car.manufacturer, car.model, car.colour, car_images.file_name FROM `car_images` inner join car on car.car_id = car_images.car_id";
+	$stmt = $mysqli->prepare($sql);
+
+	if($stmt->execute())
+	{
+		// store result
+        $cars = $stmt->get_result();
+	}
+
+}
+
+?>
 <!doctype html>
 
 <html lang="en">
@@ -13,11 +35,27 @@
 	 crossorigin="anonymous">
 	<link rel="icon" href="favicon.ico">
 	<meta name="theme-color" content="#7952b3">
+
 	<style>
 		@media (min-width: 768px) {
 			.mt-0 {
 				margin-top: 20px !important;
 			}
+		}
+
+		.wishListDiv span {
+			display: none;
+			position: absolute;
+			bottom: 0;
+			left: 0;
+			right: 0;
+		}
+
+		.wishListDiv:hover span {
+			display: block;
+			position: relative;
+			right: 5px;
+			font-size: 12px;
 		}
 	</style>
 </head>
@@ -30,70 +68,59 @@
 	</header>
 	<main>
 		<div class="container mt-0">
-			<div class="row">
-				<div class="col-sm-4">
-					<div class="card">
-						<img src="./images/stock_car1.jpg" class="card-img-top" alt="..." width="125" height="250">
-						<div class="card-body">
-							<h5 class="card-title">Special title treatment</h5>
-							<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
-						</div>
-					</div>
-				</div>
-				<div class="col-sm-4">
-					<div class="card">
-						<img src="./images/stock_car2.jpg" class="card-img-top" alt="..." width="125" height="250">
-						<div class="card-body">
-							<h5 class="card-title">Special title treatment</h5>
-							<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
-						</div>
-					</div>
-				</div>
-				<div class="col-sm-4">
-					<div class="card">
-						<img src="./images/stock_car3.jpg" class="card-img-top" alt="..." width="125" height="250">
-						<div class="card-body">
-							<h5 class="card-title">Special title treatment</h5>
-							<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
-						</div>
-					</div>
-				</div>
-			</div>
-			<div class="row">
-				<div class="col-sm-4">
-					<div class="card">
-						<img src="./images/stock_car4.jpg" class="card-img-top" alt="..." width="125" height="250">
-						<div class="card-body">
-							<h5 class="card-title">Special title treatment</h5>
-							<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
-						</div>
-					</div>
-				</div>
-				<div class="col-sm-4">
-					<div class="card">
-						<img src="./images/stock_car5.jpg" class="card-img-top" alt="..." width="125" height="250">
-						<div class="card-body">
-							<h5 class="card-title">Special title treatment</h5>
-							<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
-						</div>
-					</div>
-				</div>
-				<div class="col-sm-4">
-					<div class="card">
-						<img src="./images/stock_car3.jpg" class="card-img-top" alt="..." width="125" height="250">
-						<div class="card-body">
-							<h5 class="card-title">Special title treatment</h5>
-							<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-							<a href="#" class="btn btn-primary">Go somewhere</a>
-						</div>
-					</div>
-				</div>
-			</div>
+			<?php //php block of code to show the cars in the database
+			if($cars == "")
+			{
+				echo '<div class="row"> No cars found!</div>';
+			}
+
+			else if ($cars->num_rows > 0){
+				//initialize the counter
+				$i=0;
+
+				//iterate through the dataset we got from the database
+				foreach($cars as $car )
+				{
+					//open a row 
+					if($i == 0) 
+						echo '<div class="row">';
+
+					//add car details
+					echo "<div class=\"col-sm-4\">
+							<div class=\"card\">
+								<div class=\"wishListDiv\">
+									<a href=\"#\" class=\"wishListAnchor\">
+										<img src=\"./images/addedToWishlist.png\" class=\"float-end\" alt=\"add to wishlist\" width=\"25px\" height=\"25px\">
+										<span class=\"float-end\">Add To Wishlist</span>
+									</a>
+								</div>
+					   	 		<img src=\"./images/" . $car['file_name'] . "\" class=\"card-img-top\" alt=\"...\" width=\"125\" height=\"250\">
+					 	 		<div class=\"card-body\">
+						 			<h5 class=\"card-title\">" . $car['year'] . " " . $car['manufacturer'] . " " . $car['model'] . " " .  $car['colour'] . "</h5>
+						 			<p><strong>$" . $car['price'] . "</strong></p>
+						 			<a href=\"#\" class=\"btn btn-primary\">More Info</a>
+								</div>
+							</div>
+						 </div>";
+
+					if($i == 2)
+						echo '</div>';
+
+					//increment the counter
+					$i++;
+
+					//reset the counter
+					if($i > 2)
+						$i=0;
+
+				}
+
+				//close the row if it hasn't been closed
+				if($cars->num_rows % 3 <> 0){
+					echo '</div>';
+				}
+			}
+		 ?>
 		</div>
 		<!-- /.container -->
 		<!-- FOOTER -->
